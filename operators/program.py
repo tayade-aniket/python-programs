ca7346968406062fd1d40a111909aa653a45eca7
Aniket Tayade <156310472+tayade-aniket@users.noreply.github.com>
2025-01-16 10:42:21 +0530

    second commit

---
+++ operators/program.py
@@ -0,0 +1,55 @@
+# Arithmetic Operators
+a = 10
+b = 5
+
+add = a + b
+sub = a - b
+mul = a * b
+div = a / b
+mod = a % b
+pow = a ** b
+
+print(add)
+print(sub)
+print(mul)
+print(div)
+print(mod)
+print(pow)
+
+# Logical Operators
+a = True
+b = False
+
+print(a and b)
+print(a or b)
+print(not a)
+
+# Bitwise Operators
+a = 10
+b = 4
+
+print(a & b)
+print(a | b)
+print(~a)
+print(a ^ b)
+print(a >>2)
+print(a<<2)
+
+
+# Assignment Operators
+a = 10
+
+b = a
+print(b)
+
+b += a
+print(b)
+
+b -= a
+print(b)
+
+b *= a
+print(b)
+
+b <<= a
+print(b)
