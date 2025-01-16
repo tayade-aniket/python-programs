c9ac8b23ce52d8c5406ca1a9de774de5a0725a91
Aniket Tayade <156310472+tayade-aniket@users.noreply.github.com>
2025-01-16 10:31:47 +0530

    first commit

---
+++ variables/program.py
@@ -0,0 +1,35 @@
+# Writing first program
+print("Namaste Duniya...!");
+
+# Use Hash (#) to add comments in Python
+
+# use Variables to store values
+age = 25
+salary = 99999.49
+name = "Kakashi Hatake"
+
+print(age)
+print(salary)
+print(name)
+
+# use type() to check the data type of the value
+name = "Itachi Uchiha"
+age = 19
+alive = False
+clan = 'Uchiha'
+iq = 4.9
+
+print(name)
+print(type(name))
+print(age)
+print(type(age))
+print(alive)
+print(type(alive))
+print(clan)
+print(type(clan))
+print(iq)
+print(type(iq))
+
+# getting input from user
+val = input("Enter your age : ");
+print("Your are " + val + " years old")
